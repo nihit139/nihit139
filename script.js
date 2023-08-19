4c685ca6ab9b6a16861997dbd266cf92e12c8a64
nihit139 <142661205+nihit139@users.noreply.github.com>
2023-08-19 15:14:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var img = document.getElementById('img');
+
+var slides=['Group 2.png','Group.png'];
+
+var Start=0;
+
+function slider(){
+    if(Start<slides.length){
+        Start=Start+1;
+    }
+    else{
+        Start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img src="+slides[Start-1]+">";
+   
+}
+setInterval(slider,2000);
